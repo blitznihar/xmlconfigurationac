dce5b860b68a316979566916eb1049bca91793a4
blitznihar <blitznihar@gmail.com>
2018-12-22 10:38:08 -0600

    Initial Commit

---
+++ src/main/java/com/blitz/springboot/basics/xmlconfigurationac/XmlconfigurationacApplication.java
@@ -0,0 +1,14 @@
+package com.blitz.springboot.basics.xmlconfigurationac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlconfigurationacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlconfigurationacApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/blitz/springboot/basics/xmlconfigurationac/XmlconfigurationacApplicationTests.java
@@ -0,0 +1,17 @@
+package com.blitz.springboot.basics.xmlconfigurationac;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XmlconfigurationacApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
